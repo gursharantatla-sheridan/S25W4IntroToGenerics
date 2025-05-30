9b35950044fa0bc68155ebfa31275fe9850db244
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-30 09:01:20 -0400

    stack and queue

---
+++ QueueExample/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace QueueExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+
+            //int num = queue[1];
+
+            Console.WriteLine("Total items: " + queue.Count);
+
+            foreach (var i in queue)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
+
+
+            int num = queue.Dequeue();
+            Console.WriteLine("num after Dequeue operation: " + num);
+
+            Console.WriteLine("\nTotal items: " + queue.Count);
+
+            foreach (var i in queue)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
+
+
+
+            num = queue.Peek();
+            Console.WriteLine("num after Peek operation: " + num);
+
+            Console.WriteLine("\nTotal items: " + queue.Count);
+
+            foreach (var i in queue)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
+        }
+    }
+}
---
+++ StackExample/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace StackExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            //int num = stack[1];
+
+            Console.WriteLine("Total items: " + stack.Count);
+
+            foreach (var i in stack)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
+
+
+
+            int num = stack.Pop();
+            Console.WriteLine("num after Pop operation: " + num);
+
+            Console.WriteLine("\n\nTotal items: " + stack.Count);
+
+            foreach (var i in stack)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
+
+
+            num = stack.Peek();
+            Console.WriteLine("num after Peek operation: " + num);
+
+            Console.WriteLine("\n\nTotal items: " + stack.Count);
+
+            foreach (var i in stack)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
+        }
+    }
+}
